b707878502fabab5a8b3f0935c865be0dfcb87c3
Shiraori Chan <hiyukiastelli@gmail.com>
2022-11-05 20:08:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Lesson1
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Health = 64;
+            student1.Happiness = 2;
+            student1.Inellegence = 50;
+            student1.Knowlege = 20;
+            student1.Fun();
+        } 
+    }
+    public class Student
+    {
+        public int Health;
+        public int Inellegence;
+        public int Happiness;
+        public int Knowlege;
+
+        public void Fun()
+        {
+           Health -= 1;
+            Happiness += 5;
+            Knowlege -= 2;
+            Console.WriteLine($"Студент потусил, его Здоровье: {Health} Интеллект: {Inellegence} Счастье: {Happiness} Знание: {Knowlege}");
+        }
+
+    }
+}
